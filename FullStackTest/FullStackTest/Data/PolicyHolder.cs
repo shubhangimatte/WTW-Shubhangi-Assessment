a40427157e9e53de2c02816773e6613b64893c3d
shubhangiMatte <shubhangimatte29@gmail.com>
2024-07-18 10:02:02 +0100

    code added

---
+++ FullStackTest/FullStackTest/Data/PolicyHolder.cs
@@ -0,0 +1,9 @@
+namespace FullStackTest.Data
+{
+    public class PolicyHolder
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
